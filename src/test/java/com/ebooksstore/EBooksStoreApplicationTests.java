f87e791655e63673efa7749669e41eada6bb9038
Boris <boris.damyanov@gmail.com>
2024-04-17 10:24:01 +0200

    first commit

---
+++ src/test/java/com/ebooksstore/EBooksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebooksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBooksStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
